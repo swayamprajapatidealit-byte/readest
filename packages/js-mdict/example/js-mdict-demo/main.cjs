937ccb0d6efe0c11458f6169917326e668d71675
swayamprajapatidealit-byte <swayam.prajapat.idealit@gmail.com>
2026-08-13 12:26:59 +0530

    Restore js-mdict and simplecc-wasm as git submodules; keep foliate-js flattened

--- packages/js-mdict/example/js-mdict-demo/main.cjs
+++
@@ -1,6 +0,0 @@
-const { MDX } = require("js-mdict");
-
-const mdict = new MDX("resources/oald7.mdx");
-
-const def = mdict.lookup("ask");
-console.log(def.definition);
--- packages/simplecc-wasm/demo/web/rsbuild.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-
-export default defineConfig({
-  output: {
-    assetPrefix: process.env['PUBLIC_URL'] || '/'
-  },
-  plugins: [pluginReact()],
-});
